5b64f569c3680512b07795eb33c658864d58563f
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-03-18 15:43:14 +0000

    fix: refactor INSURED_APPOINTMENT_LIST_PATH to use INSURED_ID_PARAM constant for consistency

--- src/appointments/adapters/constants/handler-routes.constant.ts
+++ src/appointments/adapters/constants/handler-routes.constant.ts
@@ -1,7 +1,7 @@
 
 export const CREATE_APPOINTMENT_PATH = "/appointments";
 export const CREATE_APPOINTMENT_ROUTE = `POST ${CREATE_APPOINTMENT_PATH}`;
-export const INSURED_APPOINTMENT_LIST_PATH = "/insureds/{insuredId}/appointments";
+export const INSURED_ID_PARAM = "insuredId";
+export const INSURED_APPOINTMENT_LIST_PATH = `/insureds/{${INSURED_ID_PARAM}}/appointments`;
 export const GET_INSURED_APPOINTMENT_LIST_ROUTE = `GET ${INSURED_APPOINTMENT_LIST_PATH}`;
 
-export const INSURED_ID_PARAM = "insuredId";

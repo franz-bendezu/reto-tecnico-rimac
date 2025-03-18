ebca43c17dcad943a2be5f2348bfeb0f191e39b2
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-03-18 02:34:20 +0000

    test: add unit tests for appointment retrieval and completion in AppointmentController

--- src/adapters/controllers/__tests__/appointment.controller.test.ts
+++ src/adapters/controllers/__tests__/appointment.controller.test.ts
@@ -1,6 +1,8 @@
 import { AppointmentController } from "../appointment.controller";
 import { IAppointmentService } from "../../../domain/services/appointment.service.interface";
-import { IAppointmentCreateSchema } from "../../interfaces/appointment.interface";
+import { IAppointmentCompleteSchema, IAppointmentCreateSchema } from "../../interfaces/appointment.interface";
+import { IBaseAppointment } from "../../../domain/interfaces/appointment";
+
 
 describe("AppointmentController", () => {
 
@@ -71,5 +73,75 @@ describe("AppointmentController", () => {
         if ('errors' in result.body) {
             expect(result.body.errors).toBeDefined();
         }
+    });
+
+    it("should get appointments by insured id and return status 200", async () => {
+        const insuredId = "12345";
+        const appointments: IBaseAppointment[] = [{
+            insuredId: "12345",
+            scheduleId: 1,
+            countryISO: "ES",
+            lastStatus: "pending"
+        }];
+        appointmentService.getAppointmentsByInsuredId.mockResolvedValueOnce(appointments);
+
+        const result = await appointmentController.getAppointmentsByInsuredId(insuredId);
+
+        expect(appointmentService.getAppointmentsByInsuredId).toHaveBeenCalledWith(insuredId);
+        expect(result).toEqual({
+            statusCode: 200,
+            body: appointments,
+        });
+    });
+
+    it("should return status 400 if insured id is invalid", async () => {
+        const insuredId = { /* invalid insured id */ };
+
+        const result = await appointmentController.getAppointmentsByInsuredId(insuredId);
+
+        expect(result.statusCode).toBe(400);
+        if ('message' in result.body) {
+            expect(result.body.message).toBeDefined();
+        }
+        if ('errors' in result.body) {
+            expect(result.body.errors).toBeDefined();
+        }
+    });
+
+    it("should complete an appointment and return status 200", async () => {
+        const data:IAppointmentCompleteSchema = { insuredId: "12345", scheduleId: 1 };
+        appointmentService.completeAppointment.mockResolvedValueOnce();
+
+        const result = await appointmentController.completeAppointment(data);
+
+        expect(appointmentService.completeAppointment).toHaveBeenCalledWith("12345", 1);
+        expect(result).toEqual({
+            statusCode: 200,
+            body: { message: "Appointment completed" },
+        });
+    });
+
+    it("should return status 400 if complete appointment data is invalid", async () => {
+        const data = { insuredId: "12345", scheduleId: "invalid" };
+
+        const result = await appointmentController.completeAppointment(data);
+
+        expect(result.statusCode).toBe(400);
+        expect(result.body.message).toBeDefined();
+        if ('errors' in result.body) {
+            expect(result.body.errors).toBeDefined();
+        }
+    });
+
+    it("should return status 500 if an unexpected error occurs during complete appointment", async () => {
+        const data = { insuredId: "12345", scheduleId: 1 };
+        appointmentService.completeAppointment.mockRejectedValueOnce(new Error("Unexpected error"));
+
+        const result = await appointmentController.completeAppointment(data);
+
+        expect(result).toEqual({
+            statusCode: 500,
+            body: { message: "Internal server error" },
+        });
     });
 });

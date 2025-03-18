d8482dd42ccc8bd46016e6355884e9fbb49ac37d
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-03-18 03:35:14 +0000

    style: format code for better readability in appointment.handler.ts

--- src/adapters/handlers/appointment.handler.ts
+++ src/adapters/handlers/appointment.handler.ts
@@ -8,7 +8,8 @@ import type {
 import { appointmentController } from "./appointment.handler.provider";
 
 export const CREATE_APPOINTMENT_ROUTE = "POST /appointments";
-export const GET_ENSURED_APPOINTMENT_LIST = "GET /ensureds/{ensuredId}/appointments";
+export const GET_ENSURED_APPOINTMENT_LIST =
+  "GET /ensureds/{ensuredId}/appointments";
 
 export const handler: APIGatewayProxyHandlerV2 | SQSHandler = async (
   event: SQSEvent | APIGatewayProxyEventV2
@@ -18,14 +19,17 @@ export const handler: APIGatewayProxyHandlerV2 | SQSHandler = async (
       await appointmentController.completeAppointment(JSON.parse(record.body));
     }
   } else if (event.routeKey === CREATE_APPOINTMENT_ROUTE) {
-    const result = await appointmentController.createAppointment(JSON.parse(event.body || "{}"));
+    const result = await appointmentController.createAppointment(
+      JSON.parse(event.body || "{}")
+    );
     return {
       statusCode: result.statusCode,
       body: JSON.stringify(result.body),
-    } satisfies APIGatewayProxyResultV2
-
+    } satisfies APIGatewayProxyResultV2;
   } else if (event.routeKey === GET_ENSURED_APPOINTMENT_LIST) {
-    const result = await appointmentController.getAppointmentsByInsuredId(event.pathParameters?.ensuredId);
+    const result = await appointmentController.getAppointmentsByInsuredId(
+      event.pathParameters?.ensuredId
+    );
     return {
       statusCode: result.statusCode,
       body: JSON.stringify(result.body),

c15f2d2e884816c2c786d6c0d88e0c0b85ebd6b0
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2025-03-18 03:00:09 +0000

    refactor: update AppointmentCountryRDSRepository and producer constructors to use specific client types

--- src/infraestructure/messasing/appointment-country.producer.ts
+++ src/infraestructure/messasing/appointment-country.producer.ts
@@ -7,8 +7,10 @@ import { IAppointmentCountryProducer } from "./appointment-country.producer.inte
 import { IAppointmentCreate } from "../../domain/interfaces/appointment-create";
 import { IAppointmentConfig } from "../config/appointment.config.interface";
 
+export type SNSClientAppointment = Pick<SNSClient, "send">;
+
 export class AppointmentCountryProducer implements IAppointmentCountryProducer {
-  constructor(private snsClient: SNSClient, private config: IAppointmentConfig) {}
+  constructor(private snsClient: SNSClientAppointment, private config: IAppointmentConfig) {}
   async sendAppointment(appointment: IAppointmentCreate): Promise<void> {
     const snsConfig = this.config;
     const params: PublishCommandInput = {
--- src/infraestructure/messasing/appointment.producer.ts
+++ src/infraestructure/messasing/appointment.producer.ts
@@ -7,8 +7,10 @@ import {
   PutEventsCommandInput,
 } from "@aws-sdk/client-eventbridge";
 
+export type EventBridgeClientAppointment = Pick<EventBridgeClient, "send">;
+
 export class AppointmentProducer implements IAppointmentProducer {
-  constructor(private eventBridgeClient: EventBridgeClient, private config: IAppointmentCountryConfig ) {}
+  constructor(private eventBridgeClient: EventBridgeClientAppointment, private config: IAppointmentCountryConfig ) {}
   async sendAppointmentCountry(appointment: IAppointmentCreate): Promise<void> {
     const eventBridgeConfig = this.config.eventBridge;
     const params:PutEventsCommandInput = {
